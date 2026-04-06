fa226256b62b70e828fe86bcfb39c53122cebc67
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-04-06 15:00:25 -0300

    ADD base conta corrente

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{// conta corrente
+
+    class ContaCorrenteUsuario
+    {
+        public string Titular { get; set; }
+
+        public string Numero { get; set; }
+
+        public decimal Saldo { get; set; }
+
+        public decimal LimiteDebito { get; set; }
+
+        public ContaCorrenteUsuario(string numero, decimal saldo, decimal limiteDebito)
+        {
+            Numero = numero;
+            Saldo = saldo;
+            LimiteDebito = limiteDebito;
+        }
+    }
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine("Bem-vindo ao sistema bancário!");
+        Console.WriteLine("------------------------------------------");
+
+
+    }
+}
