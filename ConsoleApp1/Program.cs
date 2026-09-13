f6ef8ba2e871f51af5c9f00b0a894dab4acc2f67
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-09-13 01:11:21 -0300

    ADICIONA funcionalidade de transferência entre contas e validação de saldo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,10 @@ internal partial class Program
         decimal saldoConta = 1000;
         decimal limiteDebito = 1200;
 
+        string usuario2 = "thi";
+        decimal saldoConta2 = 12000;
+        decimal limiteDebito2 = 6000;
+
         do
         {
 
@@ -17,19 +21,31 @@ internal partial class Program
             Console.WriteLine("1 - sacar");
             Console.WriteLine("2 - depositar");
             Console.WriteLine("3 - consultar saldo");
+            System.Console.WriteLine("4 - Trasferir");
             Console.WriteLine("S - sair");
             Console.WriteLine("------------------------------------------");
             string? opcao = Console.ReadLine()?.ToLower();
 
             if (opcao == "s")
                 break;
+
             else if (opcao == "1")
             {
                 System.Console.WriteLine("quanto deseja sacar:");
                 decimal quantoSacar = Convert.ToDecimal(Console.ReadLine());
 
-                saldoConta -= quantoSacar;
+                if (saldoConta <= -limiteDebito)
+                {
+                    System.Console.WriteLine("O valor Limite de debito da foi ultapassado!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    saldoConta -= quantoSacar;
+                }
+
             }
+
             else if (opcao == "2")
             {
                 System.Console.WriteLine("valor do deposito:");
@@ -37,9 +53,30 @@ internal partial class Program
 
                 saldoConta += quantoDeposito;
             }
+
             else if (opcao == "3")
             {
                 System.Console.WriteLine($"saldo da conta {saldoConta}");
+            }
+
+            else if (opcao == "4")
+            {
+                System.Console.WriteLine("Digite o valor que deseja transferir:");
+                decimal valorTrasferencia = Convert.ToDecimal(Console.ReadLine());
+
+                // validação do saldo da conta
+                if (saldoConta <= valorTrasferencia)
+                {
+                    System.Console.WriteLine("saldo insuficiente");
+                }
+                else
+                {
+                    saldoConta -= valorTrasferencia;
+                    saldoConta2 += valorTrasferencia;
+
+                    System.Console.WriteLine($"O valor de {valorTrasferencia} foi trasferido");
+                    Console.ReadLine();
+                }
             }
 
         } while (true);
